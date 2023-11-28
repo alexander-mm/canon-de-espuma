248c79628ed4ef35c9a6f93113b152bfc75ad7e4
alexander-mm <alexanderwolftrader@gmail.com>
2023-11-27 19:21:16 -0500

    videos-to-hosting

--- src/components/02-MainScreen/Main.jsx
+++ src/components/02-MainScreen/Main.jsx
@@ -12,7 +12,7 @@ const Main = () => (
             playsInline
             loading="lazy"
         >
-            <source src="https://www.masqueunefecto.com/wp-content/uploads/2023/11/main-mobile.mp4" type="video/mp4" />
+            <source src="https://www.masqueunefecto.com/wp-content/uploads/2023/11/main.mp4" type="video/mp4" />
             Tu navegador no admite la reproducción de videos.
         </video>
 
@@ -25,7 +25,7 @@ const Main = () => (
             playsInline
             loading="lazy"
         >
-            <source src="https://www.masqueunefecto.com/wp-content/uploads/2023/11/main.mp4" type="video/mp4" />
+            <source src="https://www.masqueunefecto.com/wp-content/uploads/2023/11/main-mobile.mp4" type="video/mp4" />
             Tu navegador no admite la reproducción de videos.
         </video>
     </section>
